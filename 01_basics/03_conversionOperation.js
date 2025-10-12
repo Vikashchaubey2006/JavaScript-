a3c3a8779fbec92d83d2ef5e77005fbc2d3f9eb1
Vikash Chaubey <230101120283@centurionuniv.edu.in>
2025-10-12 15:38:54 +0530

    Conversion of detatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "22"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+
+// "22" => 22 (string to number)
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false; 
+// "" => false
+// "vikash" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
