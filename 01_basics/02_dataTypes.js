93ba5d98cd05ab2ae85cd6f0a59f87d6b968f97c
Vikash Chaubey <230101120283@centurionuniv.edu.in>
2025-10-12 15:18:33 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3
+  +
+  3) // code readibility should  be high
+
+  console.log("Vikash")
+
+
+let name  = "Vikash Chaubey"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 0 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// ubdefined => 
+// symbol => unique
+
+
+console.log(typeof age);
+
