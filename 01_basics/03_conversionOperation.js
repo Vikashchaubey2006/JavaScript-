ef83f5ead2a6c91f8f9a96fde154336560ac6e98
Vikash Chaubey <230101120283@centurionuniv.edu.in>
2025-10-12 15:57:42 +0530

    Conversion of detatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,7 +20,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" => false
 // "vikash" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************** Operations ***************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Vikash"
+let str2 = "Baba"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3 );
+
+console.log(true);
+
