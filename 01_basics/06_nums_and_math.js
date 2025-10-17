5585725cf7d455a7bacffe5b466795dd2145104b
Vikash Chaubey <230101120283@centurionuniv.edu.in>
2025-10-17 17:30:38 +0530

    nums and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,14 +1,14 @@
 const score = 400;
 
 const balance = new Number(100);
-// console.log(balance);
+console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(3));
+ console.log(otherNumber.toPrecision(3));
 
 const hundreds = 100000000000000
 // console.log(hundreds.toLocaleString('en-IN'));
@@ -23,11 +23,11 @@ const hundreds = 100000000000000
 // console.log(Math.min(4, 3, 5, 8));
 // console.log(Math.max(4, 3, 5, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()* 10) + 1);
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()* 10) + 1);
 
-const min=10
-const max=20
+// const min=10
+// const max=20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
