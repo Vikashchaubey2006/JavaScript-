a7298bf713836dae487d202c789e435bd61a8534
Vikash Chaubey <230101120283@centurionuniv.edu.in>
2025-11-05 20:33:22 +0530

    while loop

---
+++ 05_iterations/while_loop.js
@@ -0,0 +1,29 @@
+// let index = 0
+// while (index <= 100) {
+//   console.log(`Value of index is ${index}`);
+//   index = index + 11
+// }
+
+
+let myArr = ["Balajee", "Aman", "Sahil", "Karan"]
+
+let arr = 0 // the index is given by user from where the array is started
+while (arr < myArr.length) {
+  // console.log(`Value of ${myArr[arr]}`);
+  arr = arr + 1
+}
+
+//  do-while loop
+
+// let score = 1
+
+// do {
+//   console.log(`Score is ${score}`);
+//   score++
+// } while (score <= 11);
+
+let score = 16 // when the value is greater than the given score in the loop then score value is print which is given by the user
+do {
+  console.log(`${score}`);
+  score++
+}while(score <= 15);
