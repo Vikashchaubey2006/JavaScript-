dafbcaa7452322ff447c0ba72b90491053a16ccf
Vikash Chaubey <230101120283@centurionuniv.edu.in>
2025-11-06 23:48:30 +0530

    four

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+myObject = {
+  js: 'javascript',
+  cpp: 'C++',
+  rb: "ruby",
+  py: "python",
+  swift: "swift"
+}
+
+for (const key in myObject) {
+  // console.log(myObject[key]);
+  // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+  console.log(programming[key]);
+}
