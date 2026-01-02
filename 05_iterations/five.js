e1676f4be294f83a1b0f3760cf7ad2e9afa051d2
Vikash Chaubey <230101120283@centurionuniv.edu.in>
2026-01-02 16:54:25 +0530

    five.js

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -11,7 +11,6 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // function printMe(item){
 //   console.log(item);
 // }
-
 // coding.forEach(printMe)
 
 coding.forEach( (item, index, arr)=> {
